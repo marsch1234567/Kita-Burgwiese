17ac652d983e5ec4c54a8fad60ee5f453ba24a5d
marsch1234567 <markus.schliep@gmail.com>
2026-04-15 15:26:16 +0200

    Update projekte.js

--- projekte.js
+++ projekte.js
@@ -15,6 +15,7 @@ const projekteDaten = [
         beschreibung: "Gemeinsam den Kita-Alltag moderner und bunter machen.",
         bild: "2.jpeg"
     },
+    {
         titel: "Fußballtrikots im Anflug!",
         beschreibung: "Große Ereignisse werfen ihre Schatten voraus – oder in diesem Fall: ihre Trikots! Wir freuen uns riesig, ankündigen zu dürfen, dass die Kids der Kindertagesstätte Burgwiese  bald in neuem Glanz auf dem Spielfeld stehen werden.Dank der großzügigen Unterstützung vom Ambulanten Pflegedienst BAERenstark  sind die neuen Fußballtrikots bereits auf dem Weg zu uns. Die Vorfreude im Team ist riesig, und wir können es kaum erwarten, die kleinen Nachwuchskicker in ihrem neuen Outfit von JAKO  flitzen zu sehen.Ein herzliches Dankeschön an das Team von BAERenstark, das dieses Projekt möglich gemacht hat! Wir halten euch auf dem Laufenden, sobald die Trikots gelandet sind und das erste Training darin startet.Bleibt sportlich!",
         bild: "Trikots.png"
